75b3d9fb9aa63c277b7a621a8df0dbc986988617
sigau <debaeckergautier@gmail.com>
2019-11-21 20:44:05 +0100

    maj accueil

--- Mat-gau/page_accueil.php
+++ Mat-gau/page_accueil.php
@@ -1,9 +1,45 @@
 <?php include 'entete.html';?>
 
     
-        <h1>Bienvenue sur Cockthèque, la bibliotheque des cocktails </h1>
-        <img src="https://www.veuveamiot.fr/user/documents/images/divers/bandeaulegislationalcoolsite.jpg">
-        <p>Blablabblablablabal alccol</p>
+    <h1>Bienvenue sur Cockthèque, la bibliotheque des cocktails </h1>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9">
+
+            <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                <img class="d-block img-fluid" src="https://www.urbankitsupply.com/wp/en/wp-content/uploads/sites/4/2018/12/Glogi-Drink-Main-image-2-900x350.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block img-fluid" src="https://cocktailseo.com/wp-content/uploads/2018/10/cocktail-seo-900x350.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block img-fluid" src="https://www.unchainedbride.wedding/wp-content/uploads/2017/08/PROSECCO-COCKTAILS--900x350.jpg" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+            </div>
+        </div> <!-- fini row-->
+    </div> <!-- fini container -->
+
+    <div class="row"> 
+
+    </div> <!-- fini row-->
+
 
     <div>
         <h2>Nos neuf meilleurs cocktails</h2>
