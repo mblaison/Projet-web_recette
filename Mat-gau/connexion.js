0485ce800a545dbc8749d63549691fa68a641c90
sigau <debaeckergautier@gmail.com>
2019-12-18 15:53:10 +0100

    connexion

--- Mat-gau/connexion.js
+++ Mat-gau/connexion.js
@@ -1,16 +1,21 @@
 window.addEventListener("DOMContentLoaded",function(){
+    var requete = new XMLHttpRequest();
     var form_inscript = document.getElementById("id_form");
     form_inscript.addEventListener("submit",function(event){
         event.preventDefault();
-        console.log("test")
         data_formulaire = new FormData(form_inscript);
-        var requete = new XMLHttpRequest();
 		requete.open("POST","ws_connexion.php");
 		requete.send(data_formulaire);
     });
 
     requete.addEventListener("load",function(event){
         console.log(requete.responseText);
+        if (requete.status == 400){
+            alert("mot de passe erroné ou non inscrit")
+        }
+
     });
+
+
 
 });
