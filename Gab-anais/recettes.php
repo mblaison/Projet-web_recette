63041392640e8eeb4177d306da9c7bd52711c21c
AJL07 <anais.julie.l@gmail.com>
2019-12-18 22:05:34 +0100

    update

--- Gab-anais/recettes.php
+++ Gab-anais/recettes.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Anaïs Louis & Gabrielle Pozo">
+    <link rel="icon" href="img/icon.png">
     
     <title>Savory</title>
 
@@ -29,14 +31,14 @@
         <div class="container-fluid">
             <!-- Logo -->
             <div class="logo">
-                <a href="./accueil.html"><img src="img/savory.png" alt=""></a>
+                <a href="./accueil.php"><img src="img/savory.png" alt=""></a>
             </div>
 
             <!-- Menu -->
             <div class="nav-menu">
                     <nav class="main-menu mobile-menu">
                         <ul>
-                            <li><a href="accueil.html">Accueil</a></li>
+                            <li><a href="accueil.php">Accueil</a></li>
                             <li><a href="recettes.php">Recettes</a></li>
                             <li><a href="recherche.php">Recherche</a></li>
                             <li><a href="addRecipe.php">Ajouter une recette</a></li>
